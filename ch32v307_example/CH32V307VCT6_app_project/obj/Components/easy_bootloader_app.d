5e94da24dcf12c6f1d89f25eed9e25762bb17488
ohb666 <166415399+ohb666@users.noreply.github.com>
2026-08-12 00:44:51 +0800

    chore: ignore generated files and local caches

--- ch32v307_example/CH32V307VCT6_app_project/obj/Components/easy_bootloader_app.d
+++
@@ -1,6 +0,0 @@
-Components/easy_bootloader_app.o: ../Components/easy_bootloader_app.c \
- ../Components/easy_bootloader_app.h ../Components/boot_config_app.h
-
-../Components/easy_bootloader_app.h:
-
-../Components/boot_config_app.h:
--- ch32v307_example/CH32V307VCT6_app_project/obj/Components/ringbuffer.d
+++
@@ -1,4 +0,0 @@
-Components/ringbuffer.o: ../Components/ringbuffer.c \
- ../Components/ringbuffer.h
-
-../Components/ringbuffer.h:
